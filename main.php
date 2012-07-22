7ebaa14ceb27fde55df06fa5485933958edb0356
Alexandru Dan <dan_lex@yahoo.com>
2012-07-22 12:43:44 +0300

    added Sort library

--- main.php
+++ main.php
@@ -2,6 +2,18 @@
 error_reporting (E_ALL);
 include 'Config/load.php';
 
+//Sort example:
+$array = array(4, 2);
+echo ("\nBefore sort:\n");
+$sortInstance = Sort_Insertion::getInstance();
+print_r($array);
+$array = $sortInstance->sort($array);
+echo ("\nAfter sort:\n");
+print_r($array);
+
+die();
+
+//Expression example:
 $evaluator = Expression_Evaluator::getInstance();
 
 $str = '(10 + 20) + (1 + 3 * 2 / (1+1))';
@@ -11,7 +23,7 @@ echo ("\n".$strPostfix."\n");
 echo($evaluator->evaluateStrPostfix($strPostfix));
 echo(' === ');
 eval('$x = '.$str.';');
-echo ($x."");
+echo ($x);
 $expressionTree = $evaluator->getConvertor()->strInfixToExpressionTree($str);
 echo(' === ');
 echo ($evaluator->evaluateExpressionTree($expressionTree));
